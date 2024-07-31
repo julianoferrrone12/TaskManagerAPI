008483c317f580b3b3e11805c3ebfe0ff2b8b532
julianoferrrone12 <julianofcaetano@gmail.com>
2024-07-31 11:08:34 -0300

    feat: Itens mappers

---
+++ Interfaces/ITaskItemRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagerAPI.Dtos.Item;
+using TaskManagerAPI.Helpers;
+using TaskManagerAPI.Models;
+
+namespace TaskManagerAPI.Interfaces
+{
+    public interface ITaskItemRepository
+    {
+        Task<List<TaskItem>> GetAllAsync(QueryObject query);
+        Task<TaskItem?> GetByIdAsync(int id);
+        Task<TaskItem> CreateAsync(TaskItem itemModel);
+        Task<TaskItem?> UpdateAsync(int id, UpdateItemRequestDto itemDto);
+        Task<TaskItem?> DeleteAsync(int id);
+    }
+}
